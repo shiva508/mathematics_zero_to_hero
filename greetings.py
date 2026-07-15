bc04bb5b2f0b8d2cb69378ff6a5c25ebad496c42
shiva508 <dasarishiva1@gmail.com>
2026-07-15 22:28:54 +0530

    Exceptions, loops, modules

---
+++ greetings.py
@@ -0,0 +1,7 @@
+from saying import hello,good_bye
+
+if __name__ == "__main__":
+    hello("World")
+    good_bye("Hell")
+
+
---
+++ saying.py
@@ -0,0 +1,5 @@
+def hello(msg):
+    print("Hello " + msg)
+
+def good_bye(msg):
+    print("Good bye" + msg)
